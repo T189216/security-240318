f15320a2b4f85cd400943adc45e1fdeed9c019f4
seoyeon <tus0926@naver.com>
2024-03-18 10:57:15 +0900

    apllication.yml

---
+++ src/main/java/com/ll/security240318/Security240318Application.java
@@ -0,0 +1,13 @@
+package com.ll.security240318;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security240318Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Security240318Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/security240318/Security240318ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.security240318;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security240318ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
